46583e0c0e5264206760bdcebafc7c181a7ee87e
Josh Feierman <josh@sqljosh.com>
2019-08-20 12:47:32 -0400

    Updates to init script and ssh agent start script.

--- init.sh
+++ init.sh
@@ -11,6 +11,7 @@ install_azcli () {
         tee /etc/apt/sources.list.d/azure-cli.list
     apt-get update
     apt-get install azure-cli
+    az extension add --name azure-cli-iot-ext
 }
 
 install_kubectl() {
@@ -34,8 +35,14 @@ install_sops() {
 }
 
 install_rke() {
-  curl -o /bin/rke -L https://github.com/rancher/rke/releases/download/v0.2.2/rke_linux-amd64
+  curl -o /bin/rke -L https://github.com/rancher/rke/releases/download/v0.2.7/rke_linux-amd64
   chmod +x /bin/rke
+}
+
+install_terraform() {
+  curl -o /tmp/terraform.zip -L https://releases.hashicorp.com/terraform/0.12.6/terraform_0.12.6_linux_amd64.zip
+  cat /tmp/terraform.zip | gunzip > /bin/terraform
+  chmod +x /bin/terraform
 }
 
 install_helm() {
@@ -49,17 +56,38 @@ install_jq() {
     apt-get install -y jq
 }
 
-while getopts 'h' c
+install_kubeps1() {
+  curl -o /bin/kube-ps1.sh -L https://raw.githubusercontent.com/jonmosco/kube-ps1/master/kube-ps1.sh
+  SOURCECMD="source /bin/kube-ps1.sh"
+  PROMPTCMD="PS1='[\u@\h:\W $(kube_ps1)]\$ '"
+  if grep --quiet ^${SOURCECMD} /home/${USER_NAME}/.bashrc; then
+    echo "Source for Kube-ps1 already exists"
+  else
+    echo ${SOURCECMD} >> /home/${USER_NAME}/.bashrc
+  fi
+  if grep -F --quiet ${PROMPTCMD} /home/${USER_NAME}/.bashrc; then
+    echo "Source for Kube-ps1 already exists"
+  else
+    echo ${PROMPTCMD} >> /home/${USER_NAME}/.bashrc
+  fi
+}
+
+while getopts 'h:u:' c
 do
     case $c in
         h) HOME_DIR=$OPTARG ;;
+        u) USER_NAME=$OPTARG ;;
     esac
 done
+
+echo "User name is $USER_NAME"
+echo "Home directory is ${HOME_DIR}"
 
 install_azcli
 install_kubectl
 install_kubectx
 install_rke
 install_helm
-
-ln -s $HOME_DIR/.ssh ~/.ssh
+install_terraform
+install_jq
+install_kubeps1
